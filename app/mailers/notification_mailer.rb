e0757dd57a6d206fa3aa841347be013a161ce186
sasaharukimedes <sasaharu3@gmail.com>
2023-05-10 23:16:57 +0900

    add link for notification

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,7 +3,7 @@ class NotificationMailer < ApplicationMailer
   def notification_email(receiver)
     @receiver = receiver
     #ここはデプロイした先のURL
-    @url = ""
+    @url = "https://gen3-kotonoha.onrender.com/notifications"
     mail(to:"#{@receiver.email}", subject: 'お手紙が届いています！')
   end
 
