3ff12339058747a4d7ba58ddebfb1962a096c263
sasaharukimedes <sasaharu3@gmail.com>
2023-12-16 21:38:27 +0900

    add rspec

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,13 @@ module Gen3Kotonoha
     # デフォルトのlocaleを日本語(:ja)にする
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    #rspec
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
   end
 end
