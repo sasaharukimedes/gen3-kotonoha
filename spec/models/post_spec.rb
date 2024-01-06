30ed1c7dc9514f8a682a6f40ae81fcc41378a369
sasaharukimedes <sasaharu3@gmail.com>
2024-01-06 16:22:33 +0900

    add creating post & reply test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -36,4 +36,56 @@ RSpec.describe Post, type: :model do
     expect(post).to be_invalid
     p post
   end
+
+  it "returns a notification by making post" do
+    user1 = User.new(
+      id: 1,
+      name: "Yuji",
+      email: "jujutsu@email.com",
+      birthday: "1995/02/03"
+    )
+    user1.save!
+    user2 = User.new(
+      id: 2,
+      name: "Deku",
+      email: "hiroaka@email.com",
+      birthday: "1998/12/13"
+    )
+    user2.save!
+    post = Post.new(
+    dear: "Hello",
+    content: "my friend",
+    from: "bff",
+    sender_id: user1.id,
+    receiver_id: user2.id
+    )
+    expect(post.create_notification_by(user1)).to eq true
+
+    p post
+  end
+
+  it "is valid to making post by user1" do
+    user1 = User.new(
+      id: 1,
+      name: "Yuji",
+      email: "jujutsu@email.com",
+      birthday: "1995/02/03"
+    )
+    user1.save!
+    user2 = User.new(
+      id: 2,
+      name: "Deku",
+      email: "hiroaka@email.com",
+      birthday: "1998/12/13"
+    )
+    user2.save!
+    post1 = Post.new(
+    dear: "Hello",
+    content: "my friend",
+    from: "bff",
+    sender_id: user1.id,
+    receiver_id: user2.id
+    )
+    expect(post1).to be_valid
+  end
 end
--- spec/models/reply_spec.rb
+++ spec/models/reply_spec.rb
@@ -29,4 +29,37 @@ RSpec.describe Reply, type: :model do
     expect(reply).to be_invalid
     p reply
   end
+  it "returns a notification by making post" do
+    user1 = User.new(
+      id: 1,
+      name: "Yuji",
+      email: "jujutsu@email.com",
+      birthday: "1995/02/03"
+    )
+    user1.save!
+    user2 = User.new(
+      id: 2,
+      name: "Deku",
+      email: "hiroaka@email.com",
+      birthday: "1998/12/13"
+    )
+    user2.save!
+    post = Post.new(
+    id: 1,
+    dear: "Hello",
+    content: "my friend",
+    from: "bff",
+    sender_id: user1.id,
+    receiver_id: user2.id
+    )
+    post.save!
+    reply = Reply.new(
+      dear: "deku",
+      content: "wello horld",
+      from: "teketeke",
+      post_id: post.id
+    )
+    expect(reply).to be_valid
+    p reply
+  end
 end
