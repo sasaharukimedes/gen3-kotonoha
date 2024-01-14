3683156d8f932198e46d260ac1b9d1be7cc7b59a
sasaharukimedes <sasaharu3@gmail.com>
2024-01-14 21:34:25 +0900

    add context in model_spec

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,32 +1,35 @@
 require 'rails_helper'
+#本来デプロイ前に確認するところを、もうデプロイして確認している関係もあってもっと細かくやるべきところをさっと書いてます。
 
 RSpec.describe Notification, type: :model do
-  it "is invalid without a visitor_id" do
-    notification = Notification.new(visitor_id: nil)
-    notification.valid?
-    expect(notification.errors[:visitor_id]).to include("を入力してください")
-    p notification
-  end
+  context "Notification wasn't created successfully" do
+    it "is invalid without a visitor_id" do
+      notification = Notification.new(visitor_id: nil)
+      notification.valid?
+      expect(notification.errors[:visitor_id]).to include("を入力してください")
+      p notification
+    end
 
-  it "is invalid without a visited_id" do
-    notification = Notification.new(visited_id: nil)
-    notification.valid?
-    expect(notification.errors[:visited_id]).to include("を入力してください")
-    p notification
-  end
+    it "is invalid without a visited_id" do
+      notification = Notification.new(visited_id: nil)
+      notification.valid?
+      expect(notification.errors[:visited_id]).to include("を入力してください")
+      p notification
+    end
 
-  it "is invalid without an action" do
-    notification = Notification.new(action: nil)
-    notification.valid?
-    expect(notification.errors[:action]).to include("を入力してください")
-    p notification
-  end
+    it "is invalid without an action" do
+      notification = Notification.new(action: nil)
+      notification.valid?
+      expect(notification.errors[:action]).to include("を入力してください")
+      p notification
+    end
 
-  it "is invalid without a checked" do
-    notification = Notification.new(checked: nil)
-    notification.valid?
-    expect(notification.errors[:checked]).to include("は一覧にありません")
-    p notification
+    it "is invalid without a checked" do
+      notification = Notification.new(checked: nil)
+      notification.valid?
+      expect(notification.errors[:checked]).to include("は一覧にありません")
+      p notification
+    end
   end
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,39 +1,44 @@
 require 'rails_helper'
+#本来デプロイ前に確認するところを、もうデプロイして確認している関係もあってもっと細かくやるべきところをさっと書いてます。
 
 RSpec.describe User, type: :model do
-  it "is valid with a name, email, and birthday" do
-    user = User.new(
-      name: "user1",
-      email: "xxx2gmail.com",
-      birthday: "2000/11/18"
-    )
-    expect(user).to be_valid
-    p user
-  end
-  it "is invalid if user's email is not unique" do
-    user1 = User.new(
-      id: "1",
-      name: "user1",
-      email: "xxx2gmail.com",
-      birthday: "2000/11/18"
-    )
-    user1.save!
-    user2 = User.new(
-      id: "2",
-      name: "user2",
-      email: "xxx2gmail.com",
-      birthday: "2012/10/14"
-    )
-    expect(user2.valid?).to be false
-    p user1
-    p user2
-  end
-  it "is invalid without a received_at" do
-    user = User.new(
-      received_at: nil
-    )
-    expect(user).to be_invalid
-    p user
+  context "User was created successfully" do
+    it "is valid with a name, email, and birthday" do
+      user = User.new(
+        name: "user1",
+        email: "xxx2gmail.com",
+        birthday: "2000/11/18"
+      )
+      expect(user).to be_valid
+      p user
+    end
   end
 
+  context "User wasn't created successfully" do
+    it "is invalid if user's email is not unique" do
+      user1 = User.new(
+        id: "1",
+        name: "user1",
+        email: "xxx2gmail.com",
+        birthday: "2000/11/18"
+      )
+      user1.save!
+      user2 = User.new(
+        id: "2",
+        name: "user2",
+        email: "xxx2gmail.com",
+        birthday: "2012/10/14"
+      )
+      expect(user2.valid?).to be false
+      p user1
+      p user2
+    end
+    it "is invalid without a received_at" do
+      user = User.new(
+        received_at: nil
+      )
+      expect(user).to be_invalid
+      p user
+    end
+  end
 end
